529e57cd1251d02e1c5c5dc1adc7db09d0aaa34e
Aryan Penikal <penikalaryan9@gmail.com>
2024-03-31 21:59:54 +0530

    Commit 1
    Adding all the files in this repo for the first time

---
+++ assets/js/type.js
@@ -0,0 +1,6 @@
+window.ityped.init(document.querySelector('.ityped'),{
+    strings: ['Body & Soul'],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
---
+++ assets/js/video-section.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.popup-vimeo').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+});
